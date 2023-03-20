11619459f7741cc884005cb5a84ffe2675d734cb
Woody3797 <woody3797@gmail.com>
2023-03-20 15:24:08 +0800

    day19 done

--- src/main/java/ibf2022/ssf/day19/controller/LoveController.java
+++ src/main/java/ibf2022/ssf/day19/controller/LoveController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -50,6 +51,12 @@ public class LoveController {
         Love love = loveService.getById(id);
         model.addAttribute("love", love);
         return "result";
+    }
+
+    @PostMapping(path = "/listpost")
+    public String listAll(Model model) throws IOException {
+        getAll(model);
+        return "list";
     }
 
 }
